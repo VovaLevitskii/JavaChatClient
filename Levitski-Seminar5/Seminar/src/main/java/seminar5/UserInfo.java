c096775bf7e049293dd89a29c6e16b230285a68f
LENOVO_Z570\Vova_Levitski <sovrimennikvrl@gmail.com>
2017-05-24 20:44:01 +0300

    Seminar05

---
+++ Levitski-Seminar5/Seminar/src/main/java/seminar5/UserInfo.java
@@ -0,0 +1,15 @@
+package seminar5;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+
+public class UserInfo {
+
+    String login;
+    String password;
+
+}
